82196a434d8031ec6720cc654bd028b3016566b6
amigin <amigin@gmail.com>
2023-07-04 22:59:09 +0300

    Added Currency Support

--- src/trx_deposit.rs
+++ src/trx_deposit.rs
@@ -1,10 +1,17 @@
-#[derive(Clone, PartialEq, ::prost::Message)]
 #[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "trx-deposit")]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct TrxDeposit {
     #[prost(string, tag = "1")]
     pub wallet_id: String,
     #[prost(string, tag = "2")]
     pub tx_id: String,
-    #[prost(sint64, tag = "3")]
+    #[prost(enumeration = "Currency", tag = "3")]
+    pub currency: i32,
+    #[prost(sint64, tag = "4")]
     pub trx_amount: i64,
+}
+#[derive(Clone, Debug, PartialEq, ::prost::Enumeration)]
+pub enum Currency {
+    Trx = 0,
+    Usdt = 1,
 }
