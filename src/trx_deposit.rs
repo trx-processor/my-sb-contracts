a9c9155026942cff95d7588a90271c28aaa22d87
amigin <amigin@gmail.com>
2023-07-04 23:08:06 +0300

    Fixing

--- src/trx_deposit.rs
+++ src/trx_deposit.rs
@@ -1,17 +1,17 @@
 #[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "trx-deposit")]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct TrxDeposit {
+pub struct TrxDepositSbContract {
     #[prost(string, tag = "1")]
     pub wallet_id: String,
     #[prost(string, tag = "2")]
     pub tx_id: String,
-    #[prost(enumeration = "Currency", tag = "3")]
+    #[prost(enumeration = "CurrencySbModel", tag = "3")]
     pub currency: i32,
     #[prost(sint64, tag = "4")]
     pub trx_amount: i64,
 }
 #[derive(Clone, Debug, PartialEq, ::prost::Enumeration)]
-pub enum Currency {
+pub enum CurrencySbModel {
     Trx = 0,
     Usdt = 1,
 }
